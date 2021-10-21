d4ca2577fa4fb6c791a337ef3a22cd8cdda8ba89
OuchattiKhadija <ouchattikhadija1@gmail.com>
2021-10-21 10:57:47 +0100

    container 2

--- lib/main.dart
+++ lib/main.dart
@@ -60,28 +60,36 @@ class Login extends StatelessWidget {
         appBar: AppBar(),
         drawer: Drawer(),
         body: Container(
-            color: Colors.amberAccent,
-            width: double.infinity,
+            //color: Colors.amberAccent,
+            width: 200,
             //margin top & right & left & bottom
-            //margin: EdgeInsets.all(10.0),
+            margin: EdgeInsets.all(30.0),
             //margin only left & right
             //  margin: EdgeInsets.only(left: 10.0, right: 10.0),
             //margin using semitric : vertical top and bottom || horizontal left and right
             // margin : EdgeInsets.symmetric(vertical: 10,horizontal: 20),
             //margin using fromLTRB
-            margin: const EdgeInsets.fromLTRB(10, 20, 10, 15),
-            padding: EdgeInsets.all(40.0),
+            //  margin: const EdgeInsets.fromLTRB(10, 20, 10, 15),
+            //   padding: EdgeInsets.all(40.0),
+            decoration: BoxDecoration(
+              color: Colors.amberAccent,
+              border: Border.all(color :Colors.black,width:3),
+              borderRadius: BorderRadius.circular(10),
+              //border in one side or 2 or ..
+              //border : Border(bottom: BorderSide(color: Colors.blue,width: 10),right: BorderSide(color: Colors.blue,width: 20) )
+            ),
             child: Text(
-              " 90000000000000000000000000000000000000000000000000000000000000\$",
+              " Hello World!",
               style: TextStyle(
-                  fontSize: 30.0,
+                  fontSize: 20.0,
                   color: Colors.blue.shade900,
                   fontWeight: FontWeight.w200,
                   letterSpacing: 1.2,
                   wordSpacing: 5.0,
-                  backgroundColor: Colors.lime,
-                  decoration: TextDecoration.lineThrough),
-              textDirection: TextDirection.ltr,
+                 // backgroundColor: Colors.lime,
+                //  decoration: TextDecoration.lineThrough
+                ),
+              //textDirection: TextDirection.ltr,
             )));
   }
 }
