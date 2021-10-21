6eeb8999e3516867b67a067123ed71edef7cabb7
OuchattiKhadija <ouchattikhadija1@gmail.com>
2021-10-21 12:02:53 +0100

    image asset

--- lib/main.dart
+++ lib/main.dart
@@ -62,52 +62,14 @@ class Login extends StatelessWidget {
         appBar: AppBar(),
         drawer: Drawer(),
         body: Container(
-            //color: Colors.amberAccent,
-            width: 400,
-            height: 200,
-            alignment: Alignment.center,
-            //margin top & right & left & bottom
-            margin: EdgeInsets.all(30.0),
-            //margin only left & right
-            //  margin: EdgeInsets.only(left: 10.0, right: 10.0),
-            //margin using semitric : vertical top and bottom || horizontal left and right
-            // margin : EdgeInsets.symmetric(vertical: 10,horizontal: 20),
-            //margin using fromLTRB
-            //  margin: const EdgeInsets.fromLTRB(10, 20, 10, 15),
-            //   padding: EdgeInsets.all(40.0),
-            decoration: BoxDecoration(color: Colors.amberAccent,
-                // border: Border.all(color :Colors.black,width:3),
-                //   borderRadius: BorderRadius.circular(10),
-                //border in one side or 2 or ..
-                //border : Border(bottom: BorderSide(color: Colors.blue,width: 10),right: BorderSide(color: Colors.blue,width: 20) ),
-                /*image: DecorationImage(
-                  fit: BoxFit.fitHeight,
-                  //image: NetworkImage("https://i.imgur.com/UAn5HHb.png"),
-                  image : AssetImage("images/flutter.png"),
-                  //repat image inaxe x
-                  repeat: ImageRepeat.repeatX,
-                )*/
-                boxShadow: [
-                  BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 20.0,
-                      spreadRadius: 3,
-                      offset: Offset(-10, 10))
-                ]),
-            child: Text(
-              " Hello World!",
-              textAlign: TextAlign.end,
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.blue.shade900,
-                fontWeight: FontWeight.w200,
-                letterSpacing: 1.2,
-                wordSpacing: 5.0,
-                // backgroundColor: Colors.lime,
-                //  decoration: TextDecoration.lineThrough
-              ),
-              //textDirection: TextDirection.ltr,
-            )));
+          margin: EdgeInsets.all(10.0),
+          child: Image.network(
+            "https://i.imgur.com/UAn5HHb.png",
+            fit: BoxFit.fill,
+            width: double.infinity,
+            height: 300,
+          ),
+        ));
   }
 }
 
