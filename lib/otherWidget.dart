e7f90536daf6561c844993658d9e92a4a33dc1f2
OuchattiKhadija <ouchattikhadija1@gmail.com>
2021-10-25 12:11:15 +0100

    ListView 2

--- lib/otherWidget.dart
+++ lib/otherWidget.dart
@@ -10,52 +10,23 @@ class Other extends StatefulWidget {
 }
 
 class _OtherState extends State<Other> {
-  var selectedCountry;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(),
         drawer: Drawer(),
         body: ListView(
+          //    scrollDirection: Axis.horizontal,
           padding: EdgeInsets.all(10),
           //reverse order
           //reverse: true,
-          //type of scrolling 
+          //type of scrolling
           physics: BouncingScrollPhysics(),
           children: [
-            Text("text 1 list"),
             Container(
               color: Colors.blueGrey,
               child: Text("TextContainer 1 "),
               height: 90,
-            ),
-            Text("DropdownButton",
-                style: TextStyle(
-                  fontSize: 20,
-                )),
-            //if we want to hide underline : put dropDown widget under deropDownButtonHideUnderline
-            DropdownButton(
-              // dropdownColor: Colors.blueGrey.shade400,
-              //  icon: Icon(Icons.countertops),
-              // underline: Divider(thickness: 0,),
-              isExpanded: true,
-              hint: Text("Select Country"),
-              items: ["MA", "FR", "UK", "UAE", "EG", "SA"]
-                  .map((e) => DropdownMenuItem(
-                        //value in ui
-                        child: Text("Country $e"),
-                        //real value in backe
-                        value: e,
-                      ))
-                  .toList(),
-              onChanged: (val) {
-                setState(() {
-                  print("$val");
-                  selectedCountry = val.toString();
-                });
-              },
-              value: selectedCountry,
             ),
             Container(
               color: Colors.red,
@@ -68,8 +39,112 @@ class _OtherState extends State<Other> {
               height: 190,
             ),
             Container(
-              color: Colors.black87,
+
+              height: 200,
+              child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.all(10),
+                  children: [
+                    Container(
+                      width : 100,
+                      color: Colors.lightGreenAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.white,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.lightGreenAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.lightBlueAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.white,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.lightBlueAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                  ]),
+            ),
+            Container(
+              color: Colors.orangeAccent,
               child: Text("TextContainer 4 "),
+              height: 190,
+            ),
+            Container(
+              color: Colors.teal,
+              child: Text("TextContainer 5 "),
+              height: 190,
+            ),
+            ListView(
+              // shrinkWrap == hight wrap content 
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+                 // scrollDirection: Axis.horizontal,
+                  padding: EdgeInsets.all(10),
+                  children: [
+                    Container(
+                      width : 100,
+                      color: Colors.lightGreenAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.white,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.lightGreenAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.lightBlueAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.white,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                    Container(
+                      width : 100,
+                      color: Colors.lightBlueAccent,
+                      child: Text("listview2 con"),
+                      height: 100,
+                    ),
+                  ]),
+            Container(
+              color: Colors.purple.shade200,
+              child: Text("TextContainer 6 "),
+              height: 190,
+            ),
+            Container(
+              color: Colors.yellow.shade600,
+              child: Text("TextContainer 7 "),
               height: 190,
             ),
           ],
