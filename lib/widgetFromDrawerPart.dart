61850e42f51420055217483811a52caa9f23e37f
OuchattiKhadija <ouchattikhadija1@gmail.com>
2021-10-29 10:24:24 +0100

    Drawer2 / App Bar

--- lib/widgetFromDrawerPart.dart
+++ lib/widgetFromDrawerPart.dart
@@ -10,12 +10,17 @@ class FromDrawerPart extends StatefulWidget {
 }
 
 class _FromDrawerPartState extends State<FromDrawerPart> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
       appBar: AppBar(
         title: Text("FromDrawer p 61"),
-        leading: Icon(Icons.arrow_back),
+        leading: IconButton(icon: Icon(Icons.arrow_back),onPressed: (){
+          print("back prssed");
+        },),
         actions: [
           IconButton(
               onPressed: () {
@@ -29,28 +34,53 @@ class _FromDrawerPartState extends State<FromDrawerPart> {
               icon: Icon(Icons.exit_to_app)),
         ],
         backgroundColor: Colors.cyan,
-        centerTitle: true,
+//        centerTitle: true,
       ),
+      drawerScrimColor: Colors.transparent,
       drawer: Drawer(
+        
         child: Column(
           children: [
             UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(child: Image.network("https://jcibenin.org/assets/site/images/user_male_circle_filled1600.png"),),
+              currentAccountPicture: CircleAvatar(
+                child: Image.network(
+                    "https://jcibenin.org/assets/site/images/user_male_circle_filled1600.png"),
+              ),
               accountName: Text("Account name"),
               accountEmail: Text("email.account@email.ex"),
             ),
-            ListTile(title: Text("Home Page"),leading: Icon(Icons.home),onTap: (){
-              print("home");
-            },subtitle: Text("About subtitle"),),
-            ListTile(title: Text("About "),leading: Icon(Icons.info_outline),onTap: (){
-              print("About ");
-            },subtitle: Text("About subtitle"),),
-            ListTile(title: Text("Help"),leading: Icon(Icons.help),onTap: (){
-              print("help");
-            },subtitle: Text("Help subtitle"),),
-            ListTile(title: Text("Log Out"),leading: Icon(Icons.exit_to_app),onTap: (){
-              print("Log out");
-            },subtitle: Text("Log Out subtitle"),),
+            ListTile(
+              title: Text("Home Page"),
+              leading: Icon(Icons.home),
+              onTap: () {
+                print("home");
+              },
+              subtitle: Text("About subtitle"),
+            ),
+            ListTile(
+              title: Text("About "),
+              leading: Icon(Icons.info_outline),
+              onTap: () {
+                print("About ");
+              },
+              subtitle: Text("About subtitle"),
+            ),
+            ListTile(
+              title: Text("Help"),
+              leading: Icon(Icons.help),
+              onTap: () {
+                print("help");
+              },
+              subtitle: Text("Help subtitle"),
+            ),
+            ListTile(
+              title: Text("Log Out"),
+              leading: Icon(Icons.exit_to_app),
+              onTap: () {
+                print("Log out");
+              },
+              subtitle: Text("Log Out subtitle"),
+            ),
           ],
         ),
       ),
@@ -58,7 +88,14 @@ class _FromDrawerPartState extends State<FromDrawerPart> {
           alignment: Alignment.center,
           width: double.infinity,
           padding: EdgeInsets.all(10),
-          child: Text("Text")),
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+             Text("Text"),
+             ElevatedButton(onPressed: (){
+                scaffoldKey.currentState!.openDrawer();
+             },child: Text("Open Drawer"),)
+          ],)),
     );
   }
 }
