6e53d3b0c58873377137875a2539cc9da6462fb7
OuchattiKhadija <ouchattikhadija1@gmail.com>
2021-10-29 15:04:56 +0100

    BottomNavigationBar

--- lib/widgetFromDrawerPart.dart
+++ lib/widgetFromDrawerPart.dart
@@ -19,116 +19,63 @@ class _FromDrawerPartState extends State<FromDrawerPart>
   }
 
   GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+  int selectedIndex = 1;
+
+  List<Widget> widgetPages = [
+    MyHomePage(),
+    Text("text2", style: TextStyle(fontSize: 20)),
+    Text("text3", style: TextStyle(fontSize: 20)),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: scaffoldKey,
       appBar: AppBar(
-        title: Text("FromDrawer p 61"),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            print("back prssed");
-          },
-        ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                print("icon1 pressed");
-              },
-              icon: Icon(Icons.access_alarm)),
-          IconButton(
-              onPressed: () {
-                print("icon2 pressed");
-              },
-              icon: Icon(Icons.exit_to_app)),
-        ],
-        backgroundColor: Colors.cyan,
-//        centerTitle: true,
-        bottom: TabBar(
-            controller: myconroller,
-            isScrollable: true,
-            indicatorColor: Colors.white,
-            indicatorWeight: 4,
-            //padding: EdgeInsets.only(bottom: 5),
-            //  labelColor: Colors.red,
-            labelStyle: TextStyle(fontSize:16,letterSpacing: 2),
-          onTap: (index){
-            print(index);
-          },
-            tabs: [
-              Tab(icon: Icon(Icons.text_snippet), child: Text("Tab   1")),
-              Tab(child: Text("Tab   2")),
-              Tab(child: Text("Tab   3")),
-              Tab(child: Text("Tab   4")),
-              Tab(child: Text("Tab   5")),
-              Tab(child: Text("Tab   6")),
-              Tab(child: Text("Tab   7")),
-              Tab(child: Text("Tab   8")),
-              Tab(child: Text("Tab   9")),
-              Tab(child: Text("Tab   10")),
-            ]),
+        title: Text("HomePage"),
       ),
-      drawerScrimColor: Colors.transparent,
-      drawer: Drawer(
-        child: Column(
-          children: [
-            UserAccountsDrawerHeader(
-              currentAccountPicture: CircleAvatar(
-                child: Image.network(
-                    "https://jcibenin.org/assets/site/images/user_male_circle_filled1600.png"),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        backgroundColor: Colors.blue.shade100,
+        selectedItemColor: Colors.blue,
+        selectedLabelStyle:
+            TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        //selectedIconTheme: IconThemeData.fallback(),
+        showUnselectedLabels: true,
+        onTap: (i) {
+          setState(() {
+            selectedIndex = i;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.info_outline,
               ),
-              accountName: Text("Account name"),
-              accountEmail: Text("email.account@email.ex"),
-            ),
-            ListTile(
-              title: Text("Home Page"),
-              leading: Icon(Icons.home),
-              onTap: () {
-                print("home");
-              },
-              subtitle: Text("About subtitle"),
-            ),
-            ListTile(
-              title: Text("About "),
-              leading: Icon(Icons.info_outline),
-              onTap: () {
-                print("About ");
-              },
-              subtitle: Text("About subtitle"),
-            ),
-            ListTile(
-              title: Text("Help"),
-              leading: Icon(Icons.help),
-              onTap: () {
-                print("help");
-              },
-              subtitle: Text("Help subtitle"),
-            ),
-            ListTile(
-              title: Text("Log Out"),
-              leading: Icon(Icons.exit_to_app),
-              onTap: () {
-                print("Log out");
-              },
-              subtitle: Text("Log Out subtitle"),
-            ),
-          ],
-        ),
+              label: "About"),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.help_center_outlined,
+              ),
+              label: "Help"),
+        ],
       ),
-      body: TabBarView(controller: myconroller, children: [
-        Text("tab1"),
-        Text("tab2"),
-        Text("tab3"),
-        Text("tab4"),
-        Text("tab5"),
-        Text("tab1"),
-        Text("tab2"),
-        Text("tab3"),
-        Text("tab4"),
-        Text("tab5"),
-      ]),
+      body: widgetPages.elementAt(selectedIndex),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+          child: Text(
+        'Flutter BottomNavigationBar Example MyHomePage',
+      ),
     );
   }
 }
